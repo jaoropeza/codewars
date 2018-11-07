92bdddc806ddcfd58f01f80f353becfd533f2233
Angel Oropeza <angel.oropeza@imexperts.com.mx>
2018-11-07 13:32:02 -0600

    DigPow with test

--- src/main/java/com/jaoropeza/codewars/DigPow.java
+++ src/main/java/com/jaoropeza/codewars/DigPow.java
@@ -1,24 +1,15 @@
 package com.jaoropeza.codewars;
 
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
-
 public class DigPow {
 
     public static long digPow7(int n, int p) {
         String num = String.valueOf(n);
         String[] numA = num.split("");
-        long result = -1;
-        long sumPowers = 0;
+        long sum = 0;
         for (int i = 0; i < numA.length; i++) {
-            sumPowers = sumPowers + (long) Math.pow(Long.parseLong(numA[i]), p + i);
+            sum = sum + (long) Math.pow(Long.parseLong(numA[i]), p + i);
         }
-        for (int i = 1; i < Math.floor(Math.sqrt(sumPowers)); i++) {
-            if (n * i == sumPowers) {
-                return i;
-            }
-        }
-        return result;
+        return sum % n == 0 ? sum / n : -1;
     }
 
     public static long digPow(int n, int p) {
@@ -31,8 +22,7 @@ public class DigPow {
             .mapToLong(digit -> (long) Math.pow(digit, cont[0]++))
             .sum();
 
-        OptionalInt first = IntStream.range(1, (int) Math.floor(Math.sqrt(sum))).filter(i -> i * n == sum).findFirst();
-        return first.isPresent() ? first.getAsInt() : -1;
+        return sum % n == 0 ? sum / n : -1;
     }
 
 }
